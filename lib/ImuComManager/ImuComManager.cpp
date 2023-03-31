c197835ab0ea9d5262b6ce04d5202c46a452d6f7
Rex <rexblade1@gmail.com>
2023-03-31 00:50:54 -0400

    Add new files for building imu com manager

---
+++ lib/ImuComManager/ImuComManager.cpp
@@ -0,0 +1,7 @@
+/*
+IMU Communication Library built for SlimeVR - class file
+Abstracts reading and writing of IMU registers into general class
+*/
+
+#include "ImuComManager.h"
+#include "I2Cdev.h"
---
+++ lib/ImuComManager/ImuComManager.h
@@ -0,0 +1,19 @@
+/*
+IMU Communication Library built for SlimeVR - header file
+Abstracts reading and writing of IMU registers into general class
+*/
+
+#ifndef IMU_COM_MANAGER
+#define IMU_COM_MANAGER
+
+class ImuComManager {
+    public:
+        ImuComManager() {}
+        ~ImuComManager() {}
+
+    private:
+
+};
+
+#endif // IMU_COM_MANAGER
+
