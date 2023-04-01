841e714a8443305e837f7cbecc7f8e64ad4d735b
Rex <rexblade1@gmail.com>
2023-04-01 00:48:45 -0400

    Lay groundwork for ImuComManager

--- lib/ImuComManager/ImuComManager.cpp
+++ lib/ImuComManager/ImuComManager.cpp
@@ -5,3 +5,59 @@ Abstracts reading and writing of IMU registers into general class
 
 #include "ImuComManager.h"
 #include "I2Cdev.h"
+#include <SPI.h>
+
+class ImuI2CManager : public ImuComManager {
+    public:
+        int8_t ImuI2CManager::readFromRegisters(uint8_t startAddress, uint8_t length, uint8_t* outBytes) {
+            for (int i = 0; i < length; i++) {
+
+            }
+            status = imu_ok;
+            return status;
+        }
+
+        int8_t ImuI2CManager::writeToRegisters(uint8_t startAddress, uint8_t length, uint8_t* inBytes) {
+            for (int i = 0; i < length; i++) {
+
+            }
+            status = imu_ok;
+            return status;
+        }
+
+        void ImuI2CManager::getBitFromByte(uint8_t* byte, uint8_t bitPosition, uint8_t* bit) {
+            *bit = ((*byte >> bitPosition) & 0x01);
+        }
+
+        void ImuI2CManager::setBitInByte(uint8_t* byte, uint8_t bitPosition, uint8_t bit) {
+            *byte |= bit << bitPosition;
+        }
+};
+
+class ImuSPIManager : public ImuComManager {
+    public:
+        int8_t ImuSPIManager::readFromRegisters(uint8_t startAddress, uint8_t length, uint8_t* outBytes) {
+            for (int i = 0; i < length; i++) {
+
+            }
+            status = imu_ok;
+            return status;
+        }
+
+        int8_t ImuSPIManager::writeToRegisters(uint8_t startAddress, uint8_t length, uint8_t* inBytes) {
+            for (int i = 0; i < length; i++) {
+
+            }
+            status = imu_ok;
+            return status;
+        }
+        void ImuSPIManager::getBitFromByte(uint8_t* byte, uint8_t bitPosition, uint8_t* bit) {
+            *bit = ((*byte >> bitPosition) & 0x01);
+        }
+
+        void ImuSPIManager::setBitInByte(uint8_t* byte, uint8_t bitPosition, uint8_t bit) {
+            *byte |= bit << bitPosition;
+        }
+};
+
+
--- lib/ImuComManager/ImuComManager.h
+++ lib/ImuComManager/ImuComManager.h
@@ -6,10 +6,25 @@ Abstracts reading and writing of IMU registers into general class
 #ifndef IMU_COM_MANAGER
 #define IMU_COM_MANAGER
 
+enum transferStatus : int8_t {
+    imu_ok = 0,
+    imu_transfer_failed
+};
+
 class ImuComManager {
     public:
-        ImuComManager() {}
+        ImuComManager() = delete;
         ~ImuComManager() {}
+
+        virtual int8_t readFromRegisters(uint8_t startAddress, uint8_t length, uint8_t* outBytes);
+        virtual int8_t writeToRegisters(uint8_t startAddress, uint8_t length, uint8_t* inBytes);
+
+        virtual void getBitFromByte(uint8_t* byte, uint8_t bitPosition, uint8_t* bit);
+        virtual void setBitInByte(uint8_t* byte, uint8_t bitPosition, uint8_t bit);
+
+    protected:
+        enum transferStatus status = imu_ok;
+        uint8_t buffer[8];
 
     private:
 
